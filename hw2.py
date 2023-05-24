68803b03bf9384d9a52a3064e716c5c123f012ac
Mansur <mansuraitqul@gmail.com>
2023-05-24 19:08:05 +0600

    update

---
+++ hw2.py
@@ -0,0 +1,29 @@
+login = input("Введите логин: ")
+password = input("Введите пароль: ")
+
+
+correct_login = "ilyas"
+correct_password = "qwerty"
+
+if login == correct_login and password == correct_password:
+    print("Authentication completed")
+else:
+    print("Invalid login or password")
+
+#################################################
+
+a = {
+    'USD': 420,
+    'EUR': 510,
+    'RUB': 5.8
+}
+
+tenge = float(input("Введите сумму в тенге: "))
+current = input("Выберите валюту (USD, EUR, RUB): ")
+
+if current in a:
+    b = a[current]
+    convert = tenge / b
+    print("Полученная сумма: {:.2f} {}".format(convert, current))
+else:
+    print("Выбрана недопустимая валюта.")
