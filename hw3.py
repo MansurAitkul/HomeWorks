42f75bfdeda7bfc9d5dbd713a5372dbfa3a0070e
Mansur <mansuraitqul@gmail.com>
2023-05-26 17:12:32 +0600

    update

--- hw3.py
+++ hw3.py
@@ -8,3 +8,19 @@ if a + b > c:
 else:
     print("Да")
 
+##############################################################
+def checking(a, b, c):
+
+    if a + b <= c:
+        return True
+    else:
+        return False
+
+a = int(input("Введите стоимость подписки: "))
+b = int(input("Введите стоимость пиццы: "))
+c = int(input("Введите зарплату: "))
+
+if checking(a, b, c):
+    print("Да")
+else:
+    print("Нет")
